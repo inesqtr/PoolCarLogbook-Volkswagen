b35d0e7058f0a0ca0d863f7d06a0ddd321070b7a
ines <inesqtr@gmail.com>
2020-02-05 13:06:41 +0000

    add create, seed and drop database files

--- Back/db/config.js
+++ Back/db/config.js
@@ -1,4 +1,7 @@
-const  mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mysql = require('mysql');
 
 const connection = mysql.createConnection(process.env.JAWSDB_URL);
 
--- Back/db/create.js
+++ Back/db/create.js
@@ -1,13 +1,49 @@
 const connection = require('./config');
 
-connection.query(`CREATE TABLE user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    passwordHash VARBINARY(160) NOT NULL
-    )`,
-(err) => {
-	if (err) console.log(err);
-	console.log('table user created');
-}
-);
+const Car = `
+    CREATE TABLE IF NOT EXISTS car (
+        id INT NOT NULL AUTO_INCREMENT,
+        licence_plate VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id)
+    );
+`;
+
+const Trip = `
+  CREATE TABLE IF NOT EXISTS trip (
+    id INT NOT NULL AUTO_INCREMENT,
+    driver VARCHAR(225) NOT NULL,
+    date DATE NOT NULL,
+    time_start TIME NOT NULL,
+    time_finish TIME NOT NULL,
+    kms_start INT,
+    kms_finish INT,
+    location_start VARCHAR(225) NOT NULL,
+    location_destination VARCHAR(225) NOT NULL,
+    observations VARCHAR(225),
+    is_finished BOOL NOT NULL DEFAULT 0,
+    car_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (car_id)
+    REFERENCES car (id)
+    )
+`;
+
+connection.query(Car, (err) => {
+    if (err) {
+        console.log(err);
+        connection.end();
+    } else {
+        console.log('car created');
+        connection.query(Trip, (err) => {
+            if (err) {
+                console.log(err);
+                connection.end();
+            } else {
+                console.log('trip created');
+                connection.end();
+            }
+        }
+        )
+    }
+})
 
