ec0db778929dbfb7b11066eb798130f5ef8e22bf
Alexandra Santos <alexandraps17@gmail.com>
2020-02-05 17:41:53 +0000

    not much

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import './App.css';
 import TripsList from './TripsList';
@@ -16,6 +16,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Link
+        to="/book">
+          /Book</Link>
+        <Link
+        to="/trips_list">
+          /TripsList</Link>
         <h1>Pool Car Log Book</h1>
         <Route
           exact
--- front/src/TripsList.js
+++ front/src/TripsList.js
@@ -5,26 +5,37 @@ import './TripList.css';
 const trips = [{
   id: 1, date: '2020-02-15', time: '15.30 - 16.30', name: 'Alexandra', destination: 'Porto',
 }, {
-  id: 1, date: '2020-02-15', time: '15.30 - 16.30', name: 'Alexandra', destination: 'Porto',
+  id: 2, date: '2020-02-15', time: '15.30 - 16.30', name: 'Alexandra', destination: 'Porto',
 },
 {
-  id: 1, date: '2020-02-15', time: '15.30 - 16.30', name: 'Alexandra', destination: 'Porto',
+  id: 3, date: '2020-02-15', time: '15.30 - 16.30', name: 'Alexandra', destination: 'Porto',
 },
 {
-  id: 1, date: '2020-02-15', time: '15.30 - 16.30', name: 'Alexandra', destination: 'Porto',
+  id: 4, date: '2020-02-15', time: '15.30 - 16.30', name: 'Alexandra', destination: 'Porto',
 }];
 
-const TripsList = () => (
+const TripsList = () => {
+
+  const tripDetails = (id) => {
+    console.log("i was clicked")
+  }
+  
+return(
   <table className="tftable" border="1">
     <thead>
+      <tr>
       <th>Date</th>
       <th>Time</th>
       <th>Name</th>
       <th>Destination</th>
       <th>Edit</th>
+      </tr>
     </thead>
     {trips.map((trip) => (
-      <tbody key={trip.id}>
+      <tbody 
+        key={trip.id}
+        onClick={() => tripDetails(trip.id)}>
+        <tr>
         <td>{trip.date}</td>
         <td>{trip.time}</td>
         <td>{trip.name}</td>
@@ -35,8 +46,9 @@ const TripsList = () => (
                   Edit
           </Link>
         </td>
+        </tr>
       </tbody>
     ))}
   </table>
-);
+)};
 export default TripsList;
